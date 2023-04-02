392b2b10ffc8972d645896db3e3e4aa582455f6d
Ana Arango <anaarango@Anas-Macbook-Air.local>
2023-04-01 17:00:04 -0700

    bro wut

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,9 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { prisma } from "../../../server/database/client"
+import EmailProvider from "next-auth/providers/email";
+
+
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,6 +14,17 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    })
     // ...add more providers here
   ],
 }
